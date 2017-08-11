a60c166a9830c4da14524847c5105d92f3ea28f0
Baljeetindia <baljeet.india@gmail.com>
2017-08-11 19:09:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/firstpage', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'firstpage.html'));
+  res.send('Heloooooooooo');
 });
 
 app.get('/ui/madi.png', function (req, res) {
